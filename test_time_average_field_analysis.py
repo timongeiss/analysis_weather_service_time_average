51cd168bba1cbe99328ef1876d25889cea30c661
tngs31 <timon.geiss@t-online.de>
2025-03-15 14:11:10 +0100

    Update test_time_average_field_analysis.py
    applied ruff on test file

--- test_time_average_field_analysis.py
+++ test_time_average_field_analysis.py
@@ -3,14 +3,14 @@ import pytest
 import time_average_field_analysis as tafa
 
 
-
 def test_generate_time_average_radiation():
-
     days = 2
     samples_per_hour = 6
     scaling_factor = 150
 
-    data, hourly_mean = tafa.generate_time_average_radiation(days=days, samples_per_hour=samples_per_hour, scaling_factor=scaling_factor)
+    data, hourly_mean = tafa.generate_time_average_radiation(
+        days=days, samples_per_hour=samples_per_hour, scaling_factor=scaling_factor
+    )
 
     # check type
     assert isinstance(data, pd.DataFrame), "data should be dataframe"
@@ -25,6 +25,6 @@ def test_generate_time_average_radiation():
     # check lenght of dataset
     expected_length = days * 24 * samples_per_hour + 1
     assert len(data) == expected_length, f"data should have {expected_length} rows"
-    
+
     # no night radiation
     assert (data["radiation"] >= 0).all(), "all radiation values are >= 0"
